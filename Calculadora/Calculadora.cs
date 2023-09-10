a48e8a3f2b76f0dabf8dcbead380ff6e779fa116
Lucas <lucasf.leal@hotmail.com>
2023-09-10 17:45:21 -0300

    case solved

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Calculadora
 {
     public class Calculadora
     {
-        
+        public void verificaPendencias(Queue<Operacoes> filaOperacoes)
+        {
+            Console.WriteLine("{0} caso(s) a ser(em) processad(s): ", filaOperacoes.Count); 
+            foreach (Operacoes operacao in filaOperacoes)
+                Console.WriteLine("{0} {1} {2}", operacao.valorA, operacao.operador, operacao.valorB);
+            Console.WriteLine();
+        }
+
         public Operacoes calcular(Operacoes operacao)
         {
             switch(operacao.operador)
@@ -11,22 +21,26 @@ namespace Calculadora
                 case '+': operacao.resultado= soma(operacao);break;
                 case '-': operacao.resultado = subtracao(operacao);break;
                 case '*': operacao.resultado = multiplicacao(operacao);break;
+                case '/': operacao.resultado = divisao(operacao); break; //implementado
                 default: operacao.resultado = 0; break;
             }
             return operacao;
         }
-        public int soma(Operacoes operacao)
+        public decimal soma(Operacoes operacao)
         {
             return operacao.valorA + operacao.valorB;
         }
-        public int subtracao(Operacoes operacao)
+        public decimal subtracao(Operacoes operacao)
         {
             return operacao.valorA - operacao.valorB;
         }
-        public int multiplicacao(Operacoes operacao)
+        public decimal multiplicacao(Operacoes operacao)
         {
             return operacao.valorA * operacao.valorB;
         }
-       
+        public decimal divisao(Operacoes operacao)
+        {
+            return operacao.valorA / operacao.valorB;
+        }
     }
 }
